30d54fd161e6bcf022fcef58b7842bc01da9c3da
Sidney Cardoso <sidneyrsc342@gmail.com>
2023-10-01 18:09:28 -0300

    DTO com Java Record

---
+++ src/main/java/med/voll/api/endereco/Endereco.java
@@ -0,0 +1,11 @@
+package med.voll.api.endereco;
+
+public record Endereco(
+        String logradouro,
+        String bairro,
+        String cep,
+        String idade,
+        String uf,
+        String complemento) {
+
+}
