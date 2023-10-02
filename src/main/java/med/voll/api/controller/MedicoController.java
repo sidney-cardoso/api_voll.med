ddb3048679295633a6ec61743728e9f5deab3466
Sidney Cardoso <sidneyrsc342@gmail.com>
2023-10-02 09:13:03 -0300

    Funcionalidade de listagem de medicos

--- src/main/java/med/voll/api/controller/MedicoController.java
+++ src/main/java/med/voll/api/controller/MedicoController.java
@@ -2,16 +2,16 @@ package med.voll.api.controller;
 
 import jakarta.validation.Valid;
 import med.voll.api.endereco.Endereco;
+import med.voll.api.medico.DadosListagemMedico;
 import med.voll.api.medico.Medico;
 import med.voll.api.medico.MedicoRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.transaction.annotation.Transactional;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import med.voll.api.medico.DadosMedico;
+
+import java.util.List;
 
 @RestController
 @RequestMapping("/medicos")
@@ -24,5 +24,10 @@ public class MedicoController {
     @Transactional
     public void cadastrar(@RequestBody @Valid DadosMedico dados) {
         repository.save(new Medico(dados));
+    }
+
+    @GetMapping
+    public List<DadosListagemMedico> listar() {
+        return repository.findAll().stream().map(DadosListagemMedico::new).toList();
     }
 }
