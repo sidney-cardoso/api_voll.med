1980d69a2e28daafba13329f368b5bb6e67073dd
Sidney Cardoso <sidneyrsc342@gmail.com>
2023-10-02 00:18:06 -0300

    Funcionalidade de cadastro de pacientes

---
+++ src/main/java/med/voll/api/controller/PacienteController.java
@@ -0,0 +1,25 @@
+package med.voll.api.controller;
+
+import jakarta.validation.Valid;
+import med.voll.api.paciente.DadosPaciente;
+import med.voll.api.paciente.Paciente;
+import med.voll.api.paciente.PacienteRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.transaction.annotation.Transactional;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("pacientes")
+public class PacienteController {
+    @Autowired
+    private PacienteRepository repository;
+
+    @PostMapping
+    @Transactional
+    public void cadastrar(@RequestBody @Valid DadosPaciente dados) {
+        repository.save(new Paciente(dados));
+    }
+}
---
+++ src/main/java/med/voll/api/paciente/Paciente.java
@@ -0,0 +1,33 @@
+package med.voll.api.paciente;
+
+import jakarta.persistence.*;
+import lombok.EqualsAndHashCode;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import med.voll.api.endereco.Endereco;
+
+@Getter
+@EqualsAndHashCode(of = "id")
+@NoArgsConstructor
+@Entity(name = "Paciente")
+@Table(name = "pacientes")
+public class Paciente {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    private String nome;
+    private String email;
+    private String cpf;
+    private String telefone;
+
+    @Embedded
+    private Endereco endereco;
+
+    public Paciente(DadosPaciente dados) {
+        this.nome = dados.nome();
+        this.email = dados.email();
+        this.telefone = dados.telefone();
+        this.cpf = dados.cpf();
+        this.endereco = new Endereco(dados.endereco());
+    }
+}
