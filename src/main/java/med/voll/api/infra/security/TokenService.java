690ebb4f6851fbf574741051a5fe8fe8e1f639b6
Sidney Cardoso <sidneyrsc342@gmail.com>
2023-10-03 15:03:40 -0300

    Validando token

--- src/main/java/med/voll/api/infra/security/TokenService.java
+++ src/main/java/med/voll/api/infra/security/TokenService.java
@@ -1,8 +1,10 @@
 package med.voll.api.infra.security;
 
 import com.auth0.jwt.JWT;
+import com.auth0.jwt.JWTVerifier;
 import com.auth0.jwt.algorithms.Algorithm;
 import com.auth0.jwt.exceptions.JWTCreationException;
+import com.auth0.jwt.exceptions.JWTVerificationException;
 import med.voll.api.domain.usuario.Usuario;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Service;
@@ -28,6 +30,23 @@ public class TokenService {
             throw new RuntimeException("Erro ao gerar o token: ", error);
         }
     }
+
+    public String getSubject(String tokenJWT) {
+
+        try {
+            var algorithm = Algorithm.HMAC256(secret);
+            return JWT.require(algorithm)
+                    .withIssuer("API Voll.med")
+                    .build()
+                    .verify(tokenJWT)
+                    .getSubject();
+
+        } catch (JWTVerificationException exception) {
+            throw new RuntimeException("Token JWT inválido ou expirado! ", exception);
+        }
+    }
+
+
     public Instant dataExpiracao() {
         return LocalDateTime.now().plusDays(2).toInstant(ZoneOffset.of("-03:00"));
     }
