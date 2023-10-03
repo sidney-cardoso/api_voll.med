b3fa5a35ac78a42910a06ee9779cf2ab4a0b51fb
Sidney Cardoso <sidneyrsc342@gmail.com>
2023-10-03 17:43:54 -0300

    Validação de integridade

--- src/main/java/med/voll/api/domain/consulta/AgendaDeConsultas.java
+++ src/main/java/med/voll/api/domain/consulta/AgendaDeConsultas.java
@@ -1,6 +1,8 @@
 package med.voll.api.domain.consulta;
 
 
+import med.voll.api.domain.ValidacaoException;
+import med.voll.api.domain.medico.Medico;
 import med.voll.api.domain.medico.MedicoRepository;
 import med.voll.api.domain.paciente.PacienteRepository;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -19,13 +21,21 @@ public class AgendaDeConsultas {
     private PacienteRepository pacienteRepository;
 
     public void agendar(DadosAgendamentoConsulta dados) {
-
+        if(!pacienteRepository.existsById(dados.idPaciente())) {
+            throw new ValidacaoException("O Id do paciente informado não pode ser encontrado");
+        }
+        if(dados.idMedico() != null && medicoRepository.existsById(dados.idMedico())) {
+            throw new ValidacaoException("O Id do médico informado não pode ser encontrado");
+        }
 
         var paciente = pacienteRepository.findById(dados.idPaciente()).get();
-        var medico = medicoRepository.findById(dados.idMedico()).get();
+        var medico = atribuirMedico(dados);
 
         var consulta = new Consulta(null, medico, paciente, dados.data());
         repository.save(consulta);
     }
 
+    private Medico atribuirMedico(DadosAgendamentoConsulta dados) {
+        return null;
+    }
 }
