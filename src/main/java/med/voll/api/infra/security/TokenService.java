6e1949cd8deef290c4087431c642d4248e080b29
Sidney Cardoso <sidneyrsc342@gmail.com>
2023-10-03 10:31:54 -0300

    Geração de tokens

---
+++ src/main/java/med/voll/api/infra/security/TokenService.java
@@ -0,0 +1,30 @@
+package med.voll.api.infra.security;
+
+import com.auth0.jwt.JWT;
+import com.auth0.jwt.algorithms.Algorithm;
+import com.auth0.jwt.exceptions.JWTCreationException;
+import med.voll.api.domain.usuario.Usuario;
+import org.springframework.stereotype.Service;
+
+import java.time.Instant;
+import java.time.LocalDateTime;
+import java.time.ZoneOffset;
+
+@Service
+public class TokenService {
+    public String gerarToken(Usuario usuario) {
+        try {
+            var algorithm = Algorithm.HMAC256("12345678");
+            return JWT.create()
+                    .withIssuer("API Voll.med")
+                    .withSubject(usuario.getLogin())
+                    .withExpiresAt(dataExpiracao())
+                    .sign(algorithm);
+        } catch (JWTCreationException error) {
+            throw new RuntimeException("Erro ao gerar o token: ", error);
+        }
+    }
+    public Instant dataExpiracao() {
+        return LocalDateTime.now().plusDays(2).toInstant(ZoneOffset.of("-03:00"));
+    }
+}
